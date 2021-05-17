de6400a6c6c8ab433afe0a2f805bcb15418cb555
Ильнур Басыров <basyrovilnur1999@mail.ru>
2021-05-17 22:58:59 +0500

    'update'

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -5,9 +5,8 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="jumbotron">
-                    <h1>Это страница категорий!</h1>
-                    <p>И пока непонятно, что тут должно быть :c</p>
-                    <p><a class="btn btn-primary btn-lg" onclick="alert('Нет')" href="#" role="button">Да</a></p>
+                    <p>Выполнил: Басыров И.З.</p>
+                    <p>Руководитель: Лукманова Н.А.</p>
                 </div>
             </div>
         </div>
